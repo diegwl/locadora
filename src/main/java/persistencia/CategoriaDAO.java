8c2886a900f8b4125263902e49399fc93fe8f3f8
diegwl <diegocastanlopes0210@outlook.com>
2024-04-01 16:09:23 -0300

    categoria dao - alterar

--- src/main/java/persistencia/CategoriaDAO.java
+++ src/main/java/persistencia/CategoriaDAO.java
@@ -9,11 +9,29 @@ public class CategoriaDAO {
     public static boolean inserir(Categoria categoria) {
         try {
             Connection conexao = Conexao.getConexao();
-            String sql = "INSERT INTO categoria (nome, tipo)" +
+            String sql = "INSERT INTO categoria (nome, tipo) " +
                         "VALUES (?, ?)";
             PreparedStatement query = conexao.prepareStatement(sql);
             query.setString(1, categoria.getNome());
             query.setString(2, String.valueOf(categoria.getTipo()));
+            query.execute();
+            query.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public static boolean alterar(Categoria categoria) {
+        try {
+            Connection con = Conexao.getConexao();
+            String sql = "UPDATE categoria SET " +
+                    "nome = ?, " +
+                    "tipo = ? " +
+                    "WHERE id = ?";
+            PreparedStatement query = con.prepareStatement(sql);
+            query.setString(1, categoria.getNome());
+            query.setString(2, String.valueOf(categoria.getTipo()));
+            query.setInt(3, categoria.getId());
             query.execute();
             query.close();
             return true;
