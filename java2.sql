4cc8fa08ccba6cd01c27c45a07d501f00e6368aa
Diego Castan Lopes <ht3025179@hto.ifsp.edu.br>
2024-04-03 19:43:24 -0300

    script de criação do banco de dados e gitignore

---
+++ java2.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Abr-2024 às 00:39
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `java2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `tipo` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefone` char(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filme`
+--
+
+CREATE TABLE `filme` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(45) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `preco` decimal(15,2) NOT NULL,
+  `numeroDias` int(11) NOT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `diretor` varchar(45) NOT NULL,
+  `duracao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itemlocacao`
+--
+
+CREATE TABLE `itemlocacao` (
+  `id` int(11) NOT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `Jogo_id` int(11) DEFAULT NULL,
+  `Filme_id` int(11) DEFAULT NULL,
+  `Locacao_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogo`
+--
+
+CREATE TABLE `jogo` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(45) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `preco` decimal(15,2) NOT NULL,
+  `numeroDias` int(11) NOT NULL,
+  `categoria` varchar(45) DEFAULT NULL,
+  `memoria` int(11) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `locacao`
+--
+
+CREATE TABLE `locacao` (
+  `id` int(11) NOT NULL,
+  `data` date DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `Cliente_id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_filme_categoria` (`categoria_id`);
+
+--
+-- Índices para tabela `itemlocacao`
+--
+ALTER TABLE `itemlocacao`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ItemLocacao_Jogo1_idx` (`Jogo_id`),
+  ADD KEY `fk_ItemLocacao_Filme1_idx` (`Filme_id`),
+  ADD KEY `fk_ItemLocacao_Locacao1_idx` (`Locacao_id`);
+
+--
+-- Índices para tabela `jogo`
+--
+ALTER TABLE `jogo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `locacao`
+--
+ALTER TABLE `locacao`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_Locacao_Cliente1_idx` (`Cliente_id`),
+  ADD KEY `fk_Locacao_Usuario` (`usuario_id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filme`
+--
+ALTER TABLE `filme`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `itemlocacao`
+--
+ALTER TABLE `itemlocacao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `jogo`
+--
+ALTER TABLE `jogo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `locacao`
+--
+ALTER TABLE `locacao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `filme`
+--
+ALTER TABLE `filme`
+  ADD CONSTRAINT `fk_filme_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`);
+
+--
+-- Limitadores para a tabela `itemlocacao`
+--
+ALTER TABLE `itemlocacao`
+  ADD CONSTRAINT `fk_ItemLocacao_Filme1` FOREIGN KEY (`Filme_id`) REFERENCES `filme` (`id`),
+  ADD CONSTRAINT `fk_ItemLocacao_Jogo1` FOREIGN KEY (`Jogo_id`) REFERENCES `jogo` (`id`),
+  ADD CONSTRAINT `fk_ItemLocacao_Locacao1` FOREIGN KEY (`Locacao_id`) REFERENCES `locacao` (`id`);
+
+--
+-- Limitadores para a tabela `locacao`
+--
+ALTER TABLE `locacao`
+  ADD CONSTRAINT `fk_Locacao_Cliente` FOREIGN KEY (`Cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Locacao_Usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
